8aea6719ed7f395590e839e57adf36c0b3e2c798
SahkanDesertHawk <28378150+SahkanDesertHawk@users.noreply.github.com>
2022-09-08 17:05:29 -0400

    Add files via upload

--- src/main/java/org/aquila/settings/Settings.java
+++ src/main/java/org/aquila/settings/Settings.java
@@ -28,6 +28,7 @@ import org.aquila.crosschain.Litecoin.LitecoinNet;
 import org.aquila.crosschain.Dogecoin.DogecoinNet;
 import org.aquila.crosschain.Digibyte.DigibyteNet;
 import org.aquila.crosschain.Ravencoin.RavencoinNet;
+import org.aquila.crosschain.PirateChain.PirateChain;
 import org.aquila.utils.EnumUtils;
 
 // All properties to be converted to JSON via JAXB
