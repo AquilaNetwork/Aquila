9a24b0a6e349d28eeca99e066b3d1d9982bddb84
SahkanDesertHawk <28378150+SahkanDesertHawk@users.noreply.github.com>
2023-03-16 07:16:33 -0400

    Add files via upload

---
+++ target/generated-sources/package-info/org/aquila/data/block/package-info.java
@@ -0,0 +1,16 @@
+// Only edit org/aquila/data/package-info.java
+// Other package-info.java files are generated using above file
+
+@XmlJavaTypeAdapters({
+	@XmlJavaTypeAdapter(
+		type = byte[].class,
+		value = org.aquila.api.Base58TypeAdapter.class
+	), @XmlJavaTypeAdapter(
+		type = java.math.BigDecimal.class,
+		value = org.aquila.api.BigDecimalTypeAdapter.class
+	)
+})
+package org.aquila.data.block;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
